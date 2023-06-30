5c3768182179b61f97abd4a9aa8165ad3c297e21
tadeusz-cirocki <tadeusz.cirocki@widelab.co>
2023-06-30 05:19:49 +0200

    streamer details styling

--- frontend/streamer-spotlight-app/src/pages/StreamerDetails.js
+++ frontend/streamer-spotlight-app/src/pages/StreamerDetails.js
@@ -1,14 +1,14 @@
 import React, {useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom';
-import {Typography, Container} from '@mui/material';
+import {Typography, Container, Box} from '@mui/material';
 import apiService from '../services/apiService';
+import './StreamerDetails.css';
 
 const StreamerDetails = () => {
   const {streamerId} = useParams();
   const [streamer, setStreamer] = useState(null);
 
   useEffect(() => {
-    // Make an API call to fetch the streamer details based on streamerId
     const fetchStreamerDetails = async () => {
       try {
         const data = await apiService.getStreamerById(streamerId);
@@ -26,15 +26,22 @@ const StreamerDetails = () => {
   }
 
   return (
-    <Container maxWidth="sm">
-      <Typography variant="h3">{streamer.name}</Typography>
-      <Typography variant="body1">Description: {streamer.description}</Typography>
-      <Typography variant="body1">Platform: {streamer.platform}</Typography>
-      <Typography variant="body1">
-        Image:
+    <Container maxWidth="md" className="streamer-container">
+      <Box className="streamer-header">
         <img
-          src={'https://static-cdn.jtvnw.net/jtv_user_pictures/asmongold-profile_image-f7ddcbd0332f5d28-300x300.png'}
-          alt="Streamer" />
+          src="https://static-cdn.jtvnw.net/jtv_user_pictures/asmongold-profile_image-f7ddcbd0332f5d28-300x300.png"
+          alt="Streamer"
+          className="streamer-image"
+        />
+        <Typography variant="h3" className="streamer-name">
+          {streamer.name}
+        </Typography>
+      </Box>
+      <Typography variant="body1" className="streamer-platform">
+        <strong>Platform:</strong> {streamer.platform}
+      </Typography>
+      <Typography variant="body1" className="streamer-description">
+        {streamer.description}
       </Typography>
     </Container>
   );
